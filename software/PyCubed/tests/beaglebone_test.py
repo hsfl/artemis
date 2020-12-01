617f0abb9db2a59c021922027a95e1b8f0193027
Mitchell Matsumori-Kelly <mtmk@hawaii.edu>
2020-12-01 11:42:18 -1000

    Changed pins for UART

--- software/PyCubed/tests/beaglebone_test.py
+++ software/PyCubed/tests/beaglebone_test.py
@@ -4,8 +4,10 @@ import time
 
 # Initialize a BeagleBone object on pins PB16/PB17
 # for UART TX/RX respectively
-tx_pin = microcontroller.pin.PB16
-rx_pin = microcontroller.pin.PB17
+#tx_pin = microcontroller.pin.PB16
+#rx_pin = microcontroller.pin.PB17
+tx_pin = microcontroller.pin.PA13
+rx_pin = microcontroller.pin.PA12
 bb = beaglebone.BeagleBone(tx_pin, rx_pin)
 
 
