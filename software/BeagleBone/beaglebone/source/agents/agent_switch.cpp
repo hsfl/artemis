6b8d430ce3d615e30dea7b5851bb6c1cc4c5e720
Kasey Hagi <khagi@hsfl.hawaii.edu>
2021-03-11 18:55:41 -1000

    agent_switch updated

--- software/BeagleBone/beaglebone/source/agents/agent_switch.cpp
+++ software/BeagleBone/beaglebone/source/agents/agent_switch.cpp
@@ -1,7 +1,6 @@
 
 #include "SimpleAgent/SimpleAgent.h"
 #include "device/GPIO.h"
-//#include "device/switch.h"
 
 #include "utility/Configuration.h"
 
@@ -25,31 +24,32 @@ using namespace rapidjson;
  * @brief (Device Request) Enables a switch
  * @param sw The switch device
  */
-void Request_Switch_On(Switch *sw);
+string Request_Switch_On(vector<string> &args, int32_t &error);
 
 /**
  * @brief (Device Request) Disables a switch
  * @param sw The switch device
  */
-void Request_Switch_Off(Switch *sw);
+//void Request_Switch_Off(Switch *sw);
+string Request_Switch_Off(vector<string> &args, int32_t &error);
 
 /**
  * @brief (Device Request) Returns the state of a switch
  * @param sw The switch device
  */
-string Request_Switch_State(Switch *sw);
+string Request_Switch_State(vector<string> &args, int32_t &error);
 
 /**
  * @brief (Agent Request) Returns a JSON-formatted list of switches
  * @return The list as a JSON-formatted string
  */
-string Request_List();
+string Request_List(int32_t &error);
 
 /**
  * @brief Returns the heater configuration JSON
  * @return The configuration JSON string
  */
-string Request_Config();
+string Request_Config(int32_t &error);
 
 // |----------------------------------------------|
 // |                   Variables                  |
@@ -62,19 +62,23 @@ SimpleAgent *agent;
 Document switch_config;
 
 //! A map of switch names to Switch devices
-std::unordered_map<std::string, Switch*> switches;
+std::unordered_map<std::string, devicestruc*> switches;
+std::unordered_map<std::string, GPIO*> gpios;
 
 // |----------------------------------------------|
 // |                 Main Function                |
 // |----------------------------------------------|
 
 int main(int argc, char** argv) {
-	
+    int32_t error;
 	// Create the agent
 	agent = new SimpleAgent(CUBESAT_AGENT_SWITCH_NAME);
 	agent->set_activity_period(SLEEP_TIME);
-    agent->AddRequest("list", Request_List, "", "Lists available switches");
-    agent->AddRequest("config", Request_Config, "", "Returns the switch configuration");
+    agent->add_request("list", Request_List, "", "Lists available switches");
+    agent->add_request("config", Request_Config, "", "Returns the switch configuration");
+    agent->add_request("on", Request_Switch_On, "switch_name", "Turns on the switch");
+    agent->add_request("off", Request_Switch_Off, "switch_name", "Turns off the switch");
+    agent->add_request("state", Request_Switch_State, "switch_name", "Gets the switch state");
 	
 	// Parse the switch configuration
 	if ( !GetConfigDocument("switches", switch_config) ) {
@@ -84,10 +88,11 @@ int main(int argc, char** argv) {
 	assert(switch_config.IsArray());
 	
 	
-	Switch *sw;
+    devicestruc *sw;
 	GPIO *gpio;
 	string switch_name, switch_pin;
 	bool default_state;
+    vector<string> soh_list;
 	
 	// Loop over the JSON array of switch configurations
 	for (auto &switch_obj : switch_config.GetArray()) {
@@ -103,38 +108,55 @@ int main(int argc, char** argv) {
 		gpio->DigitalWrite(default_state ? GPIOValue::High : GPIOValue::Low);
 		
 		// Create the switch device
-		sw = agent->NewDevice<Switch>(switch_name);
+//		sw = agent->NewDevice<Switch>(switch_name);
+        sw = agent->add_device(switch_name, DeviceType::SWCH, error);
 		
 		// Store the switch configuration
-		sw->SetCustomProperty<std::string>("pin", switch_pin);
-		sw->SetCustomProperty<GPIO*>("handler", gpio);
+        //sw->SetCustomProperty<std::string>("pin", switch_pin);
+        //sw->SetCustomProperty<GPIO*>("handler", gpio);
 		
 		// Post some properties
-		sw->Post(sw->utc = Time::Now());
-		sw->Post(sw->voltage = default_state ? 3.3 : 0.0);
-		sw->Post(sw->enabled = false);
+        sw->utc = Time::Now();
+        sw->volt = default_state ? 3.3 : 0.0;
+        sw->enabled = false;
+//		sw->Post(sw->utc = Time::Now());
+//		sw->Post(sw->volt = default_state ? 3.3 : 0.0);
+//		sw->Post(sw->enabled = false);
 		
-		// Add device requests
-        sw->AddRequest("on", Request_Switch_On, "\n\t\tTurns on the switch");
-        sw->AddRequest("off", Request_Switch_Off, "\n\t\tTurns off the switch");
-        sw->AddRequest("state", Request_Switch_State, "\n\t\tReturns the state of the switch");
+//		// Add device requests
+//        sw->AddRequest("on", Request_Switch_On, "\n\t\tTurns on the switch");
+//        sw->AddRequest("off", Request_Switch_Off, "\n\t\tTurns off the switch");
+//        sw->AddRequest("state", Request_Switch_State, "\n\t\tReturns the state of the switch");
 		
 		// Store the switch device by name
 		switches[switch_name] = sw;
+        gpios[switch_name] = gpio;
 		
 		
 		// Add the switch to the telemetry log
-		agent->GetLog().RegisterDevice(switch_name, sw)
-				.RegisterProperty("utc", sw->utc)
-				.RegisterProperty("enabled", sw->enabled)
-				.RegisterProperty("voltage", sw->voltage)
-				.RegisterProperty("current", sw->current)
-				.RegisterProperty("power", sw->power);
+        string soh;
+        soh = agent->get_soh_name(switch_name, "utc", error);
+        soh_list.push_back(soh);
+        soh = agent->get_soh_name(switch_name, "enabled", error);
+        soh_list.push_back(soh);
+        soh = agent->get_soh_name(switch_name, "volt", error);
+        soh_list.push_back(soh);
+        soh = agent->get_soh_name(switch_name, "amp", error);
+        soh_list.push_back(soh);
+        soh = agent->get_soh_name(switch_name, "power", error);
+        soh_list.push_back(soh);
+//		agent->GetLog().RegisterDevice(switch_name, sw)
+//				.RegisterProperty("utc", sw->utc)
+//				.RegisterProperty("enabled", sw->enabled)
+//				.RegisterProperty("voltage", sw->voltage)
+//				.RegisterProperty("current", sw->current)
+//				.RegisterProperty("power", sw->power);
 	}
 	
 	
 	// Finish up the initialization
-	agent->Finalize();
+    //agent->Finalize();
+    agent->set_sohstring(soh_list);
 	agent->DebugPrint();
 	
 	// Run the main loop for this agent
@@ -143,21 +165,14 @@ int main(int argc, char** argv) {
 		
 		// Update switch info
 		for (auto pair : switches) {
-			gpio = pair.second->GetCustomProperty<GPIO*>("handler");
+            string switch_name = pair.first;
+            gpio = gpios[switch_name];
 			
 			pair.second->utc = Time::Now();
 			pair.second->enabled = gpio->DigitalRead() == GPIOValue::High;
-			pair.second->voltage = pair.second->enabled ? 3.3 : 0;
+            pair.second->volt = pair.second->enabled ? 3.3 : 0;
 		}
 	}
-	
-	
-	// Delete GPIO handlers
-	for (auto pair : switches)
-		delete pair.second->GetCustomProperty<GPIO*>("handler");
-	
-	delete agent;
-	
 	
 	return 0;
 }
@@ -168,38 +183,72 @@ int main(int argc, char** argv) {
 // |                   Requests                   |
 // |----------------------------------------------|
 
-void Request_Switch_On(Switch *sw) {
-	GPIO *gpio = sw->GetCustomProperty<GPIO*>("handler");
-	gpio->DigitalWrite(GPIOValue::High);
-	sw->utc = Time::Now();
-	sw->enabled = true;
-	sw->voltage = 3.3;
+string Request_Switch_On(vector<string> &args, int32_t &error) {
+    if(args.size() == 0) {
+        error = ErrorNumbers::COSMOS_GENERAL_ERROR_EMPTY;
+        return "\"usage: switch_name\"";
+    }
+    string switch_name = args[0];
+
+    if(gpios.find(switch_name) == gpios.end()){
+        error = ErrorNumbers::COSMOS_GENERAL_ERROR_NAME;
+        return "\"unknown switch\"";
+    }
+    GPIO *gpio = gpios[switch_name];
+    gpio->DigitalWrite(GPIOValue::High);
+    switches[switch_name]->utc = Time::Now();
+    switches[switch_name]->enabled = false;
+    switches[switch_name]->volt = 3.3;
+
+    return "\"Success\"";
 }
 
-void Request_Switch_Off(Switch *sw) {
-	GPIO *gpio = sw->GetCustomProperty<GPIO*>("handler");
+string Request_Switch_Off(vector<string> &args, int32_t &error)
+{
+    if(args.size() == 0) {
+        error = ErrorNumbers::COSMOS_GENERAL_ERROR_EMPTY;
+        return "\"usage: switch_name\"";
+    }
+    string switch_name = args[0];
+    if(gpios.find(switch_name) == gpios.end()){
+        error = ErrorNumbers::COSMOS_GENERAL_ERROR_NAME;
+        return "\"unknown switch\"";
+    }
+    GPIO *gpio = gpios[switch_name];
 	gpio->DigitalWrite(GPIOValue::Low);
-	sw->utc = Time::Now();
-	sw->enabled = false;
-	sw->voltage = 0;
+    switches[switch_name]->utc = Time::Now();
+    switches[switch_name]->enabled = false;
+    switches[switch_name]->volt = 0;
+
+    return "\"Success\"";
 }
 
-string Request_Switch_State(Switch *sw) {
-	return sw->enabled ? "on" : "off";
+string Request_Switch_State(vector<string> &args, int32_t &error) {
+    if(args.size() == 0) {
+        error = ErrorNumbers::COSMOS_GENERAL_ERROR_EMPTY;
+        return "\"usage: switch_name\"";
+    }
+    string switch_name = args[0];
+    if(gpios.find(switch_name) == gpios.end()){
+        error = ErrorNumbers::COSMOS_GENERAL_ERROR_NAME;
+        return "\"unknown switch\"";
+    }
+    return switches[switch_name]->enabled ? "\"on\"" : "\"off\"";
 }
 
-string Request_List() {
+string Request_List(int32_t &error) {
 	stringstream ss;
-	GPIO *handler;
+    GPIO *gpio;
 	
 	// Generate JSON-formatted switch info
 	ss << "{";
 	for (auto pair : switches) {
-		handler = pair.second->GetCustomProperty<GPIO*>("handler");
+        string switch_name = pair.first;
+        gpio = gpios[switch_name];
 		
 		ss << '"' << pair.first << "\": {" << std::endl;
 		ss <<	"\"enabled\": " << (pair.second->enabled ? "true" : "false") << ", " << std::endl;
-		ss <<	"\"pin\": \"" << handler->GetPinKey() << "\"" << std::endl;
+        ss <<	"\"pin\": \"" << gpio->GetPinKey() << "\"" << std::endl;
 		ss << "},";
 	}
 	ss << "}";
@@ -207,7 +256,7 @@ string Request_List() {
 	return ss.str();
 }
 
-string Request_Config() {
+string Request_Config(int32_t &error) {
 	string config;
 	GetConfigString("switches", config);
 	return config;
